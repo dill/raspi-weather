18860e9ed6eadf9d9a2f49f5d1c928cc98e3df0c
David Lawrence Miller <github@ninepointeightone.net>
2021-04-10 14:48:18 +0100

    re-arrange icons and text

--- update_rot.py
+++ update_rot.py
@@ -28,35 +28,33 @@ def display_weather(today, sunrise, sunset, hightemp, lowtemp, ico_path, tide_st
     font = ImageFont.truetype('/usr/share/fonts/truetype/lato/Lato-Heavy.ttf', 18)
     font_med = ImageFont.truetype('/usr/share/fonts/truetype/lato/Lato-Heavy.ttf', 14)
 
-    # add weather icon
-    image.paste(transparent_to_white(ico_path), (0, 0))
-
     # date
-    draw.text((70, 5), today, font = font, fill = 0)
+    draw.text((70, 2), today, font = font, fill = 0)
 
+    # sunrise/sunset
+    image.paste(transparent_to_white("icons/sunrise.png"), (70, 40))
+    draw.text((98, 45), sunrise, font = font_med, fill = 0)
+    image.paste(transparent_to_white("icons/sunset.png"), (140, 40))
+    draw.text((170, 45), sunset, font = font_med, fill = 0)
+
+    # add weather icon
+    image.paste(transparent_to_white(ico_path), (-5, -5))
 
     # temperature
-    image.paste(transparent_to_white('icons/1F321.png'), (-20, 75))
-    draw.text((35, 77), hightemp, font = font_med, fill = 0)
-    draw.text((35, 125), lowtemp, font = font_med, fill = 0)
+    image.paste(transparent_to_white('icons/1F321.png'), (-20, 65))
+    draw.text((35, 65), hightemp, font = font_med, fill = 0)
+    draw.text((35, 115), lowtemp, font = font_med, fill = 0)
 
     # tides
     for i in range(len(tide_height)):
-        draw.text((88, 52+i*20), tide_height[i], font = font_med, fill = 0)
-        image.paste(transparent_to_white("icons/water.png"), (120, 50+i*20))
-        image.paste(transparent_to_white("icons/arrow.png").rotate(tide_stat[i]), (140, 48+i*20))
-        draw.text((160, 52+i*20), tide_time[i], font = font_med, fill = 0)
-    
-
-    # sunrise/sunset
-    image.paste(transparent_to_white("icons/sunrise.png"), (0, 150))
-    draw.text((28, 155), sunrise, font = font_med, fill = 0)
-    image.paste(transparent_to_white("icons/sunset.png"), (100, 150))
-    draw.text((130, 155), sunset, font = font_med, fill = 0)
+        draw.text((108, 92+i*20), tide_height[i], font = font_med, fill = 0)
+        image.paste(transparent_to_white("icons/water.png"), (140, 90+i*20))
+        image.paste(transparent_to_white("icons/arrow.png").rotate(tide_stat[i]), (160, 88+i*20))
+        draw.text((180, 92+i*20), tide_time[i], font = font_med, fill = 0)
 
     # moon phase icon
     image.paste(transparent_to_white(moon_ico_path), (220, 130))
-    
+
     # we've built a landscape image, need to rotate it into place
     image = image.rotate(270, expand=1)
     epd.display_frame(epd.get_frame_buffer(image))
